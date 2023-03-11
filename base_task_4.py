a33d4fc48cb4d2dae8b8365562841d51a8a5952a
Pavel <sivec_p@mail.ru>
2023-03-11 22:04:22 +0300

    Add changes in base_task_4

--- base_task_4.py
+++ base_task_4.py
@@ -1,9 +1,12 @@
 revenue = int(input('Введите выручку фирмы: '))
 costs = int(input('Введите издержки фирмы: '))
-quant = int(input('Введите численность сотрудников фирмы: '))
-income = revenue - costs
-rentab = income / revenue
-unit_income = income / quant
-print(f'Финансовый результат - прибыль: {income}')
-print(f'Рентабельность выручки: {rentab}')
-print(f'Прибыль фирмы в расчете на одного сотрудника: {unit_income}')
+if revenue > costs:
+    print('Прибыль — выручка больше издержек')
+    print(f'Финансовый результат - прибыль: {revenue - costs}')
+    rentab = (revenue - costs) / revenue
+    print(f'Рентабельность выручки: {rentab}')
+    quant = int(input('Введите численность сотрудников фирмы: '))
+    print(f'Прибыль фирмы в расчете на одного сотрудника: {(revenue - costs) / quant}')
+else:
+    print('Убыток — издержки больше выручки')
+    print(f'Финансовый результат - убыток: {costs - revenue}')
